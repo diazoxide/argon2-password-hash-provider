c180b54bd0dd2911fce31374c6eff3d3c6d206e2
Aaron Yordanyan <aaron.yor@gmail.com>
2022-11-28 10:08:58 +0400

    Added platform on docker run

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-docker run -it --rm --name argon2-password-hash-provider -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean package
+docker run --platform=linux/amd64 -it --rm --name argon2-password-hash-provider -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean package
 echo "cp target/argon2-password-hash-provider-2.0.1.jar [your]/keycloak/deployments/"
 #cp target/argon2-password-hash-provider-2.0.1.jar ../../../../dxp-services/keycloak/deployments/
